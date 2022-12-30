8d91ba5e2fef782362e2c3aef4eb721fc45482f2
milo-progra <energyfabian12@gmail.com>
2022-12-30 14:30:58 -0300

    reparcion error al crear usuario

--- usuario/admin.py
+++ usuario/admin.py
@@ -1,8 +1,34 @@
 from django.contrib import admin
 from .models import Tipo_Usuario, Usuario
 from django.contrib.auth.admin import UserAdmin as BaseUserAdmin
-
+from .forms import AdminFormaActualizar, AdminFormaCreacionUsuario
 # Register your models here.
+class UserAdmin(BaseUserAdmin):
+    
+    form = AdminFormaActualizar
+    add_form = AdminFormaCreacionUsuario
 
-admin.site.register(Usuario, BaseUserAdmin)
+
+    #list_display = ('email', 'username')
+    list_filter = ('email',)
+    fieldsets = (
+        (None,{'fields': ('username','email', 'password')}),
+        ('Informacion personal', {'fields': ( 'first_name', 'last_name', 'tipo_usuario')}),
+        ('Permisos Django', {'fields': ('is_staff', 'is_active')})
+
+    )
+
+    add_fieldsets = (
+        (None, {
+            'classes':('wide',),
+            'fields':('username','password1', 'password2')
+        }),
+    )
+
+    search_fields = ('email',)
+    ordering = ('email',)
+
+
+
+admin.site.register(Usuario, UserAdmin)
 admin.site.register(Tipo_Usuario)
--- usuario/forms.py
+++ usuario/forms.py
@@ -1,6 +1,7 @@
 from django import forms
 from .models import *
 from django import forms
+from django.contrib.auth.forms import ReadOnlyPasswordHashField
 from app.models import Preregistro, Estado_animo
 
 
@@ -51,3 +52,43 @@ class FormEmociones(forms.ModelForm):
         widgets={
             'animo': forms.RadioSelect(),
         }        
+
+
+class AdminFormaCreacionUsuario(forms.ModelForm):
+    password1 = forms.CharField(label='Contraseña', widget=forms.PasswordInput)
+    password2 = forms.CharField(
+        label='Confirmar contraseña', widget=forms.PasswordInput)
+
+    class Meta:
+        model = Usuario
+        fields = ('username', 'first_name', 'last_name',
+                  'email', 'password')
+
+    def clean_password2(self):
+        password1 = self.cleaned_data.get('password1')
+        password2 = self.cleaned_data.get('password2')
+        if password1 and password2 and password1 != password2:
+            raise forms.ValidationError("Las contraseñas no coinciden")
+
+        return password2
+
+    def save(self, commit=True):
+        usuario = super(AdminFormaCreacionUsuario, self).save(commit=False)
+        usuario.set_password(self.cleaned_data["password1"])
+        if commit:
+            usuario.save()
+        return usuario
+
+
+
+class AdminFormaActualizar(forms.ModelForm):
+    # variable para que el admin solo pueda ver la contraseña
+    password = ReadOnlyPasswordHashField()
+
+    class Meta:
+        model = Usuario
+        fields = ('username', 'first_name', 'last_name',
+                  'email', 'password', 'password')
+
+    def clean_password(self):
+        return self.initial['password']
