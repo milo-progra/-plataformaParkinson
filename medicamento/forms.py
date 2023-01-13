9827daaf91c6882417c2c54a44e4a78103cea7c9
milo-progra <energyfabian12@gmail.com>
2023-01-13 08:47:04 -0300

    correcion formulario

--- medicamento/forms.py
+++ medicamento/forms.py
@@ -43,36 +43,7 @@ class FormMedicamentosFull(forms.ModelForm):
     class Meta:
         model = MedicamentosFull
 
-        fields =[
-            # 'username_paciente',
-            'laboratorio',
-            'marca',
-            'nombre_medicamento',
-            'medida_medicamento',
-            'cantidad_comprimidos',
-            'tipo_farmaco',
-            'forma_farmaceutica',
-            'recomendacion',
-            'via_ingesta',
-            
-        ]
+        fields = '__all__'
 
-        labels={
-            # 'username_paciente':'Paciente',
-            'horario_medicamento' : "Horario Medicamento",
-            'medicamento': "Medicamento",
-            'dosis' :"Dosis",
-
-
-            'nombre_medicamento': "Nombre Medicamento",
-            'medida_medicamento': "Medida Medicamento",
-            'cantidad_comprimidos':"Cantidad Comprimidos",
-            'tipo_farmaco':"Tipo Farmaco",
-            'forma_farmaceutica':"Forma Farmaceutica",
-            'recomendacion':"Recomendacion",
-            'marca':"Marca",
-            'via_ingesta':"Via Ingesta",
-            'laboratorio':"Laboratorio",
-        }
-
+       
 
