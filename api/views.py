f340e6e9cf8cbd6af556914aeae6a1fe47944cdf
milo-progra <energyfabian12@gmail.com>
2023-01-30 08:40:17 -0300

    put global con api rest

--- api/views.py
+++ api/views.py
@@ -1,26 +1,82 @@
+
 from django.shortcuts import render
 from medicamento.models import MedicamentosFull 
+from django.http.response import JsonResponse
+from rest_framework.parsers import JSONParser 
+from rest_framework import status
 from rest_framework import generics
+from rest_framework.decorators import api_view
+from rest_framework.response import Response
+from rest_framework.views import APIView
+
 # Create your views here.
 from .models import *
 from rest_framework import viewsets, permissions
+
+
 from .serializers import *
 
-class MedicamentoFullViewSet(viewsets.ModelViewSet):
-    queryset = MedicamentosFull.objects.all()
+# class MedicamentoFullViewSet(viewsets.ModelViewSet):
+#     queryset = MedicamentosFull.objects.all()
+#     #permission_classes =[permissions.IsAdminUser]
+#     serializer_class= MedicamentoFullSerializer
+
+#     #funcion para filtrar medicamentos por sucursal
+#     def get_queryset(self):
+#         #La variable medicamento guardara todo los medicamentos
+#         medicamentos = MedicamentosFull.objects.all()
+#         #Guardamos el parametro de la sucursal definida en la url
+#         sucursal = self.request.GET.get('sucursal')
+#         # si se definio el parametro sucursal en la url
+#         if sucursal:
+#             medicamentos = medicamentos.filter(sucursal = sucursal)
+#         return medicamentos
+
+#     #Esta es la funcion put
+#     def update(self, request, *args, **kwargs):
+#         partial = kwargs.pop('partial', False)
+#         instance = self.get_object()
+#         serializer = self.get_serializer(instance, data=request.data, partial=partial)
+#         serializer.is_valid(raise_exception=True)
+#         self.perform_update(serializer)
+
+#         return Response(serializer.data)
+#------------ Pruebas  api rest ----------------------------------------------------------------
+
+class MedicamentoFullViewSet(APIView):
     #permission_classes =[permissions.IsAdminUser]
     serializer_class= MedicamentoFullSerializer
-    def get_queryset(self):
+    #funcion para filtrar medicamentos por sucursal
+    def get(self, request,pk=None):
         #La variable medicamento guardara todo los medicamentos
         medicamentos = MedicamentosFull.objects.all()
         #Guardamos el parametro de la sucursal definida en la url
-        sucursal = self.request.GET.get('sucursal')
-        # si se definio el parametro sucursal en la url
+        sucursal = request.GET.get('sucursal')
+        print('!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!')       
+        #imprimo el body
+        print(request.data)
+        print(sucursal)
         if sucursal:
-            medicamentos = medicamentos.filter(sucursal = sucursal)
-        
-        return medicamentos
+            medicamentos = MedicamentosFull.objects.filter(sucursal = sucursal)
+        # si se definio el parametro sucursal en la 
+        #many = True vendran varios registros
+        return Response(MedicamentoFullSerializer(medicamentos, many = True).data)
 
-
-
-                       
+    def put(self, request,pk=None):
+        #Guardar el parametro ingresado en la url con el nombre /?sucursal
+        sucursal = request.GET.get('sucursal')
+    
+        if not sucursal:
+            return Response([])
+        medicamentos = MedicamentosFull.objects.filter(sucursal = sucursal) 
+        #guardo en la variable el query params del body con el nombre stockDiario
+        arrayStockDiario = request.data.get("stockDiario")
+        print(request.data)
+        b = 0
+        for medicamento in medicamentos:
+            print(arrayStockDiario[b])
+            medicamento.stockDiario = arrayStockDiario[b]   
+            b = b +1
+            medicamento.save()
+        # si se definio el parametro sucursal en la url
+        return Response(MedicamentoFullSerializer(medicamentos, many = True).data)
--- medicamento/admin.py
+++ medicamento/admin.py
@@ -1,5 +1,12 @@
 from django.contrib import admin
 from .models import *
+
+
+
+class AdminMedicamento(admin.ModelAdmin):
+    list_display = ('sucursal', 'id_medicamento', 'nombre_medicamento', 'medida_medicamento' ,'cantidad_comprimidos','stockSistema', 'stockDiario', 'timestamp', 'fecha_actu_stock')
+
+
 # Register your models here.
 admin.site.register(Tipo_farmaco)
 admin.site.register(Forma_farmaceutica)
@@ -8,4 +15,4 @@ admin.site.register(Marca)
 admin.site.register(Recomendacion_consumo)
 admin.site.register(Laboratorio)
 admin.site.register(Via_ingesta)
-admin.site.register(MedicamentosFull)
+admin.site.register(MedicamentosFull, AdminMedicamento)
